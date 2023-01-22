a13ae409913fe74d1d1c45ef398ff6047bd56230
khanforhad <dev.forhadkhan@gmail.com>
2023-01-22 23:16:31 +0600

    TEST API

--- JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
+++ JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
@@ -29,17 +29,5 @@ namespace JenkinsWebApplicationDemo.Controllers
             })
             .ToArray();
         }
-
-        [HttpGet(Name = "fayaz-bio")]
-        public IEnumerable<WeatherForecast> Bio()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
